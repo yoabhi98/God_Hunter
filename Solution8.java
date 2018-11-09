865792a86a95382e0fa5af34119b3a9f324bc361
Abhishek Mishra <42784341+yoabhi98@users.noreply.github.com>
2018-11-09 12:45:12 +0530

    Add files via upload

---
+++ Solution8.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution8 {
+
+    public static void main(String[] args) 
+	{
+	int i,j,k,sum=0;
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] b = new int[n];
+        for ( i = 0; i < n; i++) 
+	b[i]=in.nextInt();
+        for (i = 0; i < n; i++)
+	{
+	 for (j = i+1; j < n; j++)
+	{
+	  sum=b[i]+b[j];
+	   for (k = 0; k < n; k++) 
+	   {
+                if (sum==b[k])           
+                   System.out.println(b[i]+" "+b[j]+" "+b[k]);
+
+	}
+		}
+			}
+		
+ 	
+		
+            
+
+    }
+}
